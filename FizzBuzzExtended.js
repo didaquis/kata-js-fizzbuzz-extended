052b3dce1722f68ca51261f21b47bc19d44ab338
Dídac García <didaquis@gmail.com>
2019-08-01 22:53:50 +0200

    Refactored code

--- FizzBuzzExtended.js
+++ FizzBuzzExtended.js
@@ -1,31 +1,22 @@
-function fizzBuzzExtended(n) {
+function fizzBuzzExtended (n) {
 	let result = '';
 
-	if (isMultipleOfThree(n)) {
-		result += 'Fizz';
-	}
-	if (isMultipleOfFive(n)) {
-		result += 'Buzz';
-	}
-	if (isMultipleOfSeven(n)) {
-		result += 'Pop';
-	}
+	if (isMultipleOfThree(n)) result += 'Fizz';
+	if (isMultipleOfFive(n)) result += 'Buzz';
+	if (isMultipleOfSeven(n)) result += 'Pop';
 
-	if(result.length) {
-		return result;
-	}
-	return n;
+	return result || n;
 }
 
-function isMultipleOfThree(n) {
+function isMultipleOfThree (n) {
 	return n % 3 === 0;
 }
 
-function isMultipleOfFive(n) {
+function isMultipleOfFive (n) {
 	return n % 5 === 0;
 }
 
-function isMultipleOfSeven(n) {
+function isMultipleOfSeven (n) {
 	return n % 7 === 0;
 }
 
